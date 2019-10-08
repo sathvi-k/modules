b03dcbb6d1dff2da993c5fda457c33bfc7fa7742
johnbkariuki <56233847+johnbkariuki@users.noreply.github.com>
2019-10-08 18:40:02 -0400

    Add files via upload

--- queue.c
+++ queue.c
@@ -11,15 +11,32 @@
 
 #include <stdint.h>
 #include <stdbool.h>
+#include "queue.h"
 
-typedef void queue_t;
-typedef struct queue_t;
-
-typedef struct queue_t{
+typedef struct intqueue_t{
 	void *front;
 	void *back;
-	void *next;
-}queue_t;
+}intqueue_t;
+
+typedef struct element_t{
+	struct element_t *next;
+	void *data;
+}element_t;
+
+
+static element_t* create_element(void *elementp){
+	                            
+ 	element_t *et;
+                                                                                
+  if(!(et=(element_t*)malloc(sizeof(element_t)))){                                       
+    printf("[Error: malloc failed allocating element]\n");                          
+    return NULL;                                                                
+  }                                                                             
+                                                                                
+  et->next=NULL;
+	et->data=elementp;
+  return et;
+}
 
 queue_t* qopen(void){
 	                            
@@ -32,19 +49,29 @@ queue_t* qopen(void){
                                                                                 
   qp->front=NULL;
 	qp->back=NULL;
-	qp->next=NULL;
-  return qp;                                                                     
+  return qp;
 }
 
 void qclose(queue_t *qp){
 	if(qp->front != NULL && qp->back !=NULL){
-		void *i
+		void *i;
 			for(i=qp->front;i!=NULL;i=i->next){
+				free(i->data);
 				free(i);
 			}
 	}
-	
+	free(qp);
 }
- 
+
+int32_t qput(queue_t *qp,void *elementp){
+	if(qp->front==NULL && qp->back=NULL){
+		qp->front=elementp;
+		qp->back=elementp;
+	}
+	else{
+		back->next=elementp;
+		
+	}
+}
 
 	
